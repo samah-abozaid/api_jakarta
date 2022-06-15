2c5b9b90dd224cb8013c28e612878b2a175e4862
samah-abozaid <samah.abozaid89@gmail.com>
2022-06-15 16:46:27 +0200

    objet annuaire dans session

--- src/main/java/com/poe/demoapi/api/AnnuaireApi.java
+++ src/main/java/com/poe/demoapi/api/AnnuaireApi.java
@@ -1,12 +1,17 @@
 
 package com.poe.demoapi.api;
 
+import com.poe.demoapi.business.Annuaire;
 import com.poe.demoapi.business.Personne;
 
 import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 @Path("/personnes")
@@ -14,15 +19,29 @@ public class AnnuaireApi {
     
  @GET()
  @Produces({MediaType.APPLICATION_JSON})
-public ArrayList<Personne> getPersonnes(){
-    
-  ArrayList<Personne>personnes=new ArrayList<>();
-  personnes.add(new Personne("jc", "dominac"));
-  return personnes;
+public ArrayList<Personne> getPersonnes(@Context HttpServletRequest request){
     
     
-}
+  Annuaire annuaire=(Annuaire)request.getSession().getAttribute("annuaire");{
+   if(annuaire==null){
+       annuaire= new Annuaire();
+   } 
+   return annuaire.getPersonnes();
+} 
+  
+}     
+      
+  
     
-    
-    
+    @POST()
+    @Consumes({ MediaType.APPLICATION_JSON })
+    public void postPersonne(Personne newPersonne, @Context HttpServletRequest request) {
+        
+        Annuaire annuaire = (Annuaire)request.getSession().getAttribute("annuaire");
+        if(annuaire == null) {
+            annuaire = new Annuaire();       
+        }
+        annuaire.addpersonne(newPersonne);
+        request.getSession().setAttribute("annuaire", annuaire);
+    }
 }
