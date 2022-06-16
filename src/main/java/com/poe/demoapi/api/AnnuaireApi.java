5084a5899b0a509466a1f5121e845befed11c549
samah-abozaid <samah.abozaid89@gmail.com>
2022-06-16 12:21:24 +0200

    crude api

--- src/main/java/com/poe/demoapi/api/AnnuaireApi.java
+++ src/main/java/com/poe/demoapi/api/AnnuaireApi.java
@@ -3,45 +3,102 @@ package com.poe.demoapi.api;
 
 import com.poe.demoapi.business.Annuaire;
 import com.poe.demoapi.business.Personne;
-
 import java.util.ArrayList;
+import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 @Path("/personnes")
 public class AnnuaireApi {
     
- @GET()
- @Produces({MediaType.APPLICATION_JSON})
-public ArrayList<Personne> getPersonnes(@Context HttpServletRequest request){
-    
-    
-  Annuaire annuaire=(Annuaire)request.getSession().getAttribute("annuaire");{
-   if(annuaire==null){
-       annuaire= new Annuaire();
-   } 
-   return annuaire.getPersonnes();
-} 
-  
-}     
-      
-  
-    
-    @POST()
-    @Consumes({ MediaType.APPLICATION_JSON })
-    public void postPersonne(Personne newPersonne, @Context HttpServletRequest request) {
+    @GET()
+    @Produces({ MediaType.APPLICATION_JSON })
+    public ArrayList<Personne> getPersonnes(@Context HttpServletRequest request) {
         
         Annuaire annuaire = (Annuaire)request.getSession().getAttribute("annuaire");
         if(annuaire == null) {
             annuaire = new Annuaire();       
         }
-        annuaire.addpersonne(newPersonne);
+        return annuaire.getPersonnes();
+    }
+    
+    @POST()
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Personne postPersonne(Personne newPersonne, @Context HttpServletRequest request) {
+        
+        Annuaire annuaire = (Annuaire)request.getSession().getAttribute("annuaire");
+        if(annuaire == null) {
+            annuaire = new Annuaire();       
+        }
+        Personne personne = annuaire.addPersonne(newPersonne);
+        request.getSession().setAttribute("annuaire", annuaire);
+        return personne;
+    }
+    
+    @Path("/{id}")
+    @GET()
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Personne getPersonne(@PathParam("id") long personneId, @Context HttpServletRequest request) {
+        
+        Annuaire annuaire = (Annuaire)request.getSession().getAttribute("annuaire");
+        if(annuaire == null) {
+            annuaire = new Annuaire();       
+        }
+        Optional<Personne> op = annuaire.getPersonne(personneId);
+        if(op.isPresent()){
+            return op.get();
+        }
+        
+        // TODO : à améliorer ?
+        return new Personne();
+    }
+    
+    @Path("/{id}")
+    @PUT()
+    @Consumes({ MediaType.APPLICATION_JSON })
+    public void putPersonne(Personne personne, @Context HttpServletRequest request){
+        Annuaire annuaire = (Annuaire)request.getSession().getAttribute("annuaire");
+        if(annuaire == null) {
+            annuaire = new Annuaire();       
+        }
+        annuaire.updatePersonne(personne);
+        request.getSession().setAttribute("annuaire", annuaire);
+    }
+    
+    
+    @Path("/{id}")
+    @DELETE()
+    public void deletePersonnne(@PathParam("id") long personneId, @Context HttpServletRequest request){
+        Annuaire annuaire = (Annuaire)request.getSession().getAttribute("annuaire");
+        if(annuaire == null) {
+            annuaire = new Annuaire();       
+        }
+        annuaire.deletePersonne(personneId);
         request.getSession().setAttribute("annuaire", annuaire);
     }
 }
+    
+   
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
--- src/main/java/com/poe/demoapi/business/Annuaire.java
+++ src/main/java/com/poe/demoapi/business/Annuaire.java
@@ -1,66 +1,70 @@
 
 package com.poe.demoapi.business;
 
-import static java.lang.ModuleLayer.empty;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Optional;
 
-
 public class Annuaire {
-    
-   private ArrayList<Personne> personnes=new ArrayList<>();
-   private long nextId=0;
-   
-   public void addpersonne(Personne newpersonne){
-   
-       newpersonne.setId(nextId);
-       nextId++;
-       personnes.add(newpersonne);
-       
-     }  
-   
-   public Optional< Personne> getpersonne(long id){
-       for(Personne p: personnes){
-           if(p.getId().equals(id))
-               return Optional.of(p);
-       }
-      //on peut faire un retour e optional return new Personne();
-      //maniere de retourner en cas au y'a pas d'objet
-      //au lieu de null
-      
-      
-       return  Optional.empty();
-       }
-       
-   public void updatepersonne(Personne personne){
-       for(Personne p: personnes){
-           if(p.getId().equals(personne.getId())){
-               personnes.remove(p);
-               personnes.add(personne);
-               
-               
-           }
-       }
-   }
-   
-   public void deletpersonne(long id){
-      for(Personne p:personnes){
-          if(p.getId().equals(id)){
-              personnes.remove(p);
-          }
-      } 
-       
-   }
-   
-   
-   
-   
-   
-   
-   
+
+    private ArrayList<Personne> personnes = new ArrayList<>();
+    private long nextId = 1;
+
+    public Personne addPersonne(Personne newPersonne) {
+        newPersonne.setId(nextId);
+        nextId++;
+        personnes.add(newPersonne);
+        return newPersonne;
+    }
 
     public ArrayList<Personne> getPersonnes() {
         return personnes;
+    }
+
+    public Optional<Personne> getPersonne(long id) {
+        for (Personne p : personnes) {
+            if (p.getId().equals(id)) {
+                return Optional.of(p);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public void updatePersonne(Personne personne) {
+
+        Iterator<Personne> it = personnes.iterator();
+        int index = 0;
+        while (it.hasNext() && index == 0) {
+            Personne p = it.next();
+            if (p.getId().equals(personne.getId())) {
+                index = personnes.indexOf(p);
+            }
+        }
+
+        personnes.set(index, personne);
+
+//        for(Personne p : personnes){
+//            if(p.getId().equals(personne.getId())){
+////                personnes.remove(p);
+////                personnes.add(personne);
+//                
+//                int index = personnes.indexOf(personne);
+//                personnes.set(index, personne);
+//            }
+//        }
+    }
+
+    public void deletePersonne(long id) {
+        Iterator<Personne> it = personnes.iterator();
+        int index = 0;
+        while (it.hasNext() && index == 0) {
+            Personne p = it.next();
+            if (p.getId().equals(id)) {
+                index = personnes.indexOf(p);
+            }
+        }
+
+        personnes.remove(index);
     }
 
     public void setPersonnes(ArrayList<Personne> personnes) {
@@ -74,6 +78,7 @@ public class Annuaire {
     public void setNextId(long nextId) {
         this.nextId = nextId;
     }
+}
    
    
    
